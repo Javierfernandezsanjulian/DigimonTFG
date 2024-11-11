f4a530e812c58285e763aa8cbe17db3a2d22bf03
Macacovis <victorav2004@gmail.com>
2024-11-11 16:17:29 +0100

    Revert "Changed both digital and physical collection activity to a single activity for best structure. Added menu to dashboard."
    This reverts commit 503cfa10849f24795eb04559122b919cdb4e74a6.

---
+++ app/src/main/java/com/example/digimontcg/PhysicalCollectionActivity.kt
@@ -0,0 +1,14 @@
+package com.example.digimontcg
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PhysicalCollectionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_physical_collection)
+
+
+    }
+}
